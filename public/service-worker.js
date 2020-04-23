0b834753d061555faabaf75a691f226f4d121374
Kevin Wilson (TAU) <kevin.wilson@tuni.fi>
2020-04-23 21:29:33 +0300

    Application v1.0 - Content v0.1

--- public/service-worker.js
+++ public/service-worker.js
@@ -24,10 +24,45 @@ const assets = [
 const content = [
   ///// Automatically generated code /////
   '/content.json',
-  '/content/example.png',
-  '/content/process1.html',
-  '/content/report1.html',
-  '/content/sop1.html'
+  '/content/lakana-sop-admin-02-en.html',
+  '/content/lakana-sop-lab-01-en.html',
+  '/content/lakana-sop-data-01-en.html',
+  '/content/lakana-sop-admin-01-en.html',
+  '/content/lakana-sop-lab-02-en.html',
+  '/content/lakana-sop-lab-03-en.html',
+  '/content/lakana-sop-lab-04-en.html',
+  '/content/lakana-sop-lab-05-en.html',
+  '/content/lakana-sop-pharm-01-en.html',
+  '/content/lakana-sop-prep-01-en.html',
+  '/content/lakana-sop-prep-02-en.html',
+  '/content/lakana-sop-prep-03-en.html',
+  '/content/lakana-sop-prep-04-en.html',
+  '/content/lakana-sop-proc-01-en.html',
+  '/content/lakana-sop-proc-02-en.html',
+  '/content/lakana-sop-proc-03-en.html',
+  '/content/lakana-sop-proc-04-en.html',
+  '/content/lakana-sop-proc-05-en.html',
+  '/content/lakana-sop-proc-06-en.html',
+  '/content/lakana-sop-proc-07-en.html',
+  '/content/lakana-sop-proc-08-en.html',
+  '/content/lakana-sop-proc-09-en.html',
+  '/content/lakana-sop-sop-01-en.html',
+  '/content/lakana-sop-visit-01-en.html',
+  '/content/lakana-sop-visit-02-en.html',
+  '/content/lakana-sop-visit-03-en.html',
+  '/content/lakana-sop-visit-04-en.html',
+  '/content/lakana-sop-visit-05-en.html',
+  '/content/lakana-sop-visit-06-en.html',
+  '/content/lakana-sop-visit-07-en.html',
+  '/content/lakana-sop-visit-08-en.html',
+  '/content/lakana-sop-visit-09-en.html',
+  '/content/lakana-sop-visit-10-en.html',
+  '/content/lakana-sop-visit-12-en.html',
+  '/content/lakana-sop-visit-11-en.html',
+  '/content/lakana-sop-visit-13-en.html',
+  '/content/lakana-sop-visit-14-en.html',
+  '/content/lakana-sop-visit-15-en.html',
+  '/content/lakana-sop-visit-16-en.html'
   ///// End of automatically generated code /////
 ];
 
@@ -61,4 +96,6 @@ self.addEventListener('fetch', (event) => {
       return cacheResponse || fetch(event.request);
     }))
 });
+
+
 
