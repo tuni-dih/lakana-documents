ab6435c64b08253c508ea46dcb01d8c051dedf2e
Kevin Wilson (TAU) <kevin.wilson@tuni.fi>
2020-05-08 13:51:51 +0300

    Updated version and enabled service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const applicationVersion = '5'
+const applicationVersion = '6'
 
 let lakanaDocumentsCache = 'lakana-documents-' + applicationVersion;
 
@@ -29,17 +29,17 @@ const content = [
   '/content/include1.html',
   '/content/lakana-sop-admin-01-en.html',
   '/content/lakana-sop-admin-02-en.html',
-  '/content/lakana-sop-lab-01-en.html',
   '/content/lakana-sop-data-01-en.html',
+  '/content/lakana-sop-lab-01-en.html',
   '/content/lakana-sop-lab-02-en.html',
   '/content/lakana-sop-lab-03-en.html',
   '/content/lakana-sop-lab-04-en.html',
+  '/content/lakana-sop-lab-05-en.html',
   '/content/lakana-sop-prep-01-en.html',
   '/content/lakana-sop-pharm-01-en.html',
   '/content/lakana-sop-prep-02-en.html',
-  '/content/lakana-sop-lab-05-en.html',
-  '/content/lakana-sop-prep-04-en.html',
   '/content/lakana-sop-prep-03-en.html',
+  '/content/lakana-sop-prep-04-en.html',
   '/content/lakana-sop-proc-01-en.html',
   '/content/lakana-sop-proc-02-en.html',
   '/content/lakana-sop-proc-03-en.html',
@@ -48,19 +48,19 @@ const content = [
   '/content/lakana-sop-proc-06-en.html',
   '/content/lakana-sop-proc-07-en.html',
   '/content/lakana-sop-proc-08-en.html',
-  '/content/lakana-sop-sop-01-en.html',
   '/content/lakana-sop-proc-09-en.html',
+  '/content/lakana-sop-sop-01-en.html',
   '/content/lakana-sop-visit-01-en.html',
   '/content/lakana-sop-visit-02-en.html',
+  '/content/lakana-sop-visit-03-en.html',
   '/content/lakana-sop-visit-04-en.html',
   '/content/lakana-sop-visit-05-en.html',
-  '/content/lakana-sop-visit-03-en.html',
   '/content/lakana-sop-visit-06-en.html',
   '/content/lakana-sop-visit-07-en.html',
   '/content/lakana-sop-visit-08-en.html',
-  '/content/lakana-sop-visit-11-en.html',
-  '/content/lakana-sop-visit-10-en.html',
   '/content/lakana-sop-visit-09-en.html',
+  '/content/lakana-sop-visit-10-en.html',
+  '/content/lakana-sop-visit-11-en.html',
   '/content/lakana-sop-visit-12-en.html',
   '/content/lakana-sop-visit-13-en.html',
   '/content/lakana-sop-visit-14-en.html',
@@ -99,6 +99,7 @@ self.addEventListener('fetch', (event) => {
       return cacheResponse || fetch(event.request);
     }))
 });
+
 
 
 
